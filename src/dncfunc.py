2bfa7b91aeb5070f22a1fb7a13d1e23571e04c12
shika <debrina.veisha@gmail.com>
2024-03-15 11:01:47 +0700

    feat: add bezier curve n points

--- src/dncfunc.py
+++ src/dncfunc.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 titik = []
+garisbantu = []
 
 def midpoint(p1, p2):
     return[(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]
@@ -12,28 +13,39 @@ def deleteganjil(list):
     for i in sorted(hapus, reverse=True):
         del list[i]
 
+def firstinitiation(control_points):
+    garban = 0
+    for i in range(len(control_points)-1):
+        garisbantu.append(midpoint(control_points[i],control_points[i+1]))
+        garban+=1
+
+    titik.append(control_points[0])
+    titik.append(garisbantu[0])
+    for j in range(1,len(garisbantu)):
+        titik.append(midpoint(garisbantu[j-1],garisbantu[j]))
+        titik.append(garisbantu[j])
+    titik.append(control_points[len(control_points)-1])
+
+    return garban
+
 num_of_iteration = int(input("Masukkan jumlah iterasi: "))
-num_of_control_points = 3
-control_points = [(0,0),(-4,2.5),(5,0.5)]
+num_of_control_points = 5
+control_points = [(0,0),(-4,2.5),(5,3),(7,0),(10,5)]
 initial_control_points = control_points.copy()  # Make a copy of the original control points
 
-garban = 0
-garisbantu = []
-garisbantu.append(midpoint(control_points[0],control_points[1]))
-garisbantu.append(midpoint(control_points[1],control_points[2]))
-garban+=2
+garban =firstinitiation(control_points)
 
-titik.append(control_points[0])
-titik.append(garisbantu[0])
-titik.append(midpoint(garisbantu[0],garisbantu[1]))
-titik.append(garisbantu[1])
-titik.append(control_points[2])
-
+print(titik)
+print(garban)
 # Function to plot Bezier curve
 def plot_bezier_curve(titik):
     plt.grid(True)
-    plt.plot([p[0] for p in initial_control_points], [p[1] for p in initial_control_points], 'bo-', label='Control Points') 
-    for i in range(0,len(garisbantu),2):
+    plt.plot([p[0] for p in initial_control_points], [p[1] for p in initial_control_points], 'bo-', label='Control Points')
+    #garban awal
+    awal = num_of_control_points-2
+    for i in range(awal):
+        plt.plot([garisbantu[i][0], garisbantu[i+1][0]], [garisbantu[i][1], garisbantu[i+1][1]], 'yo-', markersize=3)
+    for i in range(awal+1,len(garisbantu),2):
         plt.plot([garisbantu[i][0], garisbantu[i+1][0]], [garisbantu[i][1], garisbantu[i+1][1]], 'yo-', markersize=3)
     for i in range(len(titik)-1):
         x_values = [titik[i][0], titik[i+1][0]]
@@ -42,8 +54,13 @@ def plot_bezier_curve(titik):
 
 # Plotting initial control points and intermediate points
 plt.plot([p[0] for p in initial_control_points], [p[1] for p in initial_control_points], 'bo-', label='Control Points') 
-for i in range(0,len(garisbantu),2):
+for i in range(num_of_control_points-2):
     plt.plot([garisbantu[i][0], garisbantu[i+1][0]], [garisbantu[i][1], garisbantu[i+1][1]], 'yo-', markersize=3)
+titik2 = titik.copy()
+deleteganjil(titik2)
+plot_bezier_curve(titik2)
+plt.pause(1) 
+plt.clf()
 
 plt.xlabel('X')
 plt.ylabel('Y')
@@ -52,7 +69,9 @@ plt.legend()
 
 # Plotting Bezier curve for each iteration
 for i in range(num_of_iteration-1):
-    plot_bezier_curve(titik)
+    titik2 = titik.copy()
+    deleteganjil(titik2)
+    plot_bezier_curve(titik2)
     plt.pause(1) 
     plt.clf()  # Clear plot for the next iteration
 
@@ -67,15 +86,15 @@ for i in range(num_of_iteration-1):
     mid1 = []
     mid2 = []
 
-    for i in range(len(bag1)//2):
-        garisbantu.append(midpoint(bag1[b1],bag1[b1+1]))
-        garisbantu.append(midpoint(bag1[b1+1],bag1[b1+2]))
-        b1+=2
+    print(bag1,bag2)
 
-    for i in range(len(bag1)//2):
+    for i in range(len(bag1)-1):
+        garisbantu.append(midpoint(bag1[b1],bag1[b1+1]))
+        b1+=1
+
+    for i in range(len(bag1)-1):
         garisbantu.append(midpoint(bag2[b2],bag2[b2+1]))
-        garisbantu.append(midpoint(bag2[b2+1],bag2[b2+2]))
-        b2+=2
+        b2+=1
 
     for i in range(garban,b1+garban,2):
         mid1.append(midpoint(garisbantu[i],garisbantu[i+1]))
@@ -85,16 +104,21 @@ for i in range(num_of_iteration-1):
         mid2.append(midpoint(garisbantu[i],garisbantu[i+1]))
         garban+=2
 
+    print(mid1,mid2)
     deleteganjil(titik)
 
     j=1
+    if(len(mid1)<len(mid2)):
+        j+=1
     for i in range(len(mid1)):
         if(mid1[i] not in titik):
             titik.insert(j,mid1[i])
             j+=2
     
     j=1
-    for i in range(len(mid1)):
+    if(len(mid2)<len(mid1)):
+        j+=1
+    for i in range(len(mid2)):
         if(mid2[i] not in titik):
             titik.insert(j+length,mid2[i])
             j+=2
@@ -106,7 +130,9 @@ for i in range(num_of_iteration-1):
         garban2+=1
 
 # Final Bezier curve
-plot_bezier_curve(titik)
+titik2 = titik.copy()
+deleteganjil(titik2)
+plot_bezier_curve(titik2)
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Final Bezier Curve')

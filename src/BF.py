cc5374eb333646503463d5b64a20199486fc0701
shika <debrina.veisha@gmail.com>
2024-03-16 17:45:57 +0700

    add main function

--- src/BF.py
+++ src/BF.py
@@ -3,55 +3,52 @@ import numpy as np
 import functions as fn
 import timeit
 
-def formula(num_of_points):
-    list = [1]       
-    for k in range(max(num_of_points,0)):             
-         list.append(list[k]*(num_of_points-k)//(k+1))             
-    return list
+class BF():
+    def formula(self, num_of_points):
+        list = [1]       
+        for k in range(max(num_of_points,0)):             
+            list.append(list[k]*(num_of_points-k)//(k+1))             
+        return list
 
-def inbetween(t,list_of_points):
-    const = formula(len(list_of_points)-1)
-    Rox = 0
-    Roy = 0
-    for i in range (len(const),0,-1):
-        Rox += const[len(const)-i] * pow((1-t),i-1) * pow(t,(len(const)-i)) * list_of_points[len(const)-i][0]
-        Roy += const[len(const)-i] * pow((1-t),i-1) * pow(t,(len(const)-i)) * list_of_points[len(const)-i][1]
-    return ((Rox,Roy))
+    def inbetween(self, t,list_of_points):
+        const = self.formula(len(list_of_points)-1)
+        Rox = 0
+        Roy = 0
+        for i in range (len(const),0,-1):
+            Rox += const[len(const)-i] * pow((1-t),i-1) * pow(t,(len(const)-i)) * list_of_points[len(const)-i][0]
+            Roy += const[len(const)-i] * pow((1-t),i-1) * pow(t,(len(const)-i)) * list_of_points[len(const)-i][1]
+        return ((Rox,Roy))
 
-num_of_control_points,control_points,num_of_iteration = fn.takeinput()
-start = timeit.default_timer()
-# temporary
-# num_of_control_points = 5
-# control_points = [(-2,-3),(-3,-2),(0,-2),(1,-3),(2,1)]
-# num_of_iteration = 20
-res = []
-t_val = np.linspace(0, 1, num_of_iteration)
+    def solvebf(self):
+        num_of_control_points,control_points,num_of_iteration = fn.takeinput()
+        start = timeit.default_timer()
+        res = []
+        t_val = np.linspace(0, 1, num_of_iteration)
 
-for i in range (len(t_val)):
-    res.append(inbetween(t_val[i],control_points))
+        for i in range (len(t_val)):
+            res.append(self.inbetween(t_val[i],control_points))
 
+        # Draw the curve
+        plt.figure()
+        points = res
+        # Iterate through the points then plot the curve
+        for i in range(1, len(points)):
+            x_values = [points[j][0] for j in range(i+1)]
+            y_values = [points[j][1] for j in range(i+1)]
+            plt.grid(True)
+            plt.xlabel('X')
+            plt.ylabel('Y')
+            plt.title('Final Bezier Curve')
+            plt.plot([p[0] for p in control_points], [p[1] for p in control_points], 'ro-', label='Control Points') 
+            plt.plot(x_values, y_values, marker='o', color='blue')
+            plt.pause(0.5) 
 
-# Draw the curve
-plt.figure()
-points = res
-# Iterate through the points then plot the curve
-for i in range(1, len(points)):
-    x_values = [points[j][0] for j in range(i+1)]
-    y_values = [points[j][1] for j in range(i+1)]
-    plt.grid(True)
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('Final Bezier Curve')
-    plt.plot([p[0] for p in control_points], [p[1] for p in control_points], 'ro-', label='Control Points') 
-    plt.plot(x_values, y_values, marker='o', color='blue')
-    plt.pause(0.5)  # Pause for a short duration
+        end = timeit.default_timer()
+        elapsed_time = end - start
 
-end = timeit.default_timer()
-elapsed_time = end - start
-
-print(str(elapsed_time) + " ms\n")
-# Plot final curve
-x_values = [point[0] for point in points]
-y_values = [point[1] for point in points]
-plt.plot(x_values, y_values, marker='o', color='blue')
-plt.show()
+        print(str(elapsed_time) + " ms\n")
+        # Plot final curve
+        x_values = [point[0] for point in points]
+        y_values = [point[1] for point in points]
+        plt.plot(x_values, y_values, marker='o', color='blue')
+        plt.show()
--- src/main.py
+++ src/main.py
@@ -1,4 +1,29 @@
 from dncfunc import Dncfunc as dn
+from BF import BF as bf
 
-kurva = dn()
-kurva.solvednc()
+while True:
+    print("Welcome to Bezier Curve Generator!")
+    print("You can choose to draw it with the methods below:")
+    print("1. Divide and Conquer (Fast and efficient for large sets of points)")
+    print("2. Brute Force (Simple and straightforward)")
+    method = input("Choose a number: ")
+
+    while method not in ['1', '2']:
+        print("Invalid Number! Please choose 1 or 2.")
+        method = input("Choose a number: ")
+
+    method = int(method)
+
+    if method == 1:
+        print("You have chosen the Divide and Conquer method.")
+        kurva = dn()
+        kurva.solvednc()
+    else:
+        print("You have chosen the Brute Force method.")
+        kurva = bf()
+        kurva.solvebf()
+
+    repeat = input("Do you want to draw another curve? (yes/no): ").lower()
+    if repeat != "yes":
+        print("Thank you for using Bezier Curve Generator. Goodbye!")
+        break
